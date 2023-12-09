cd0de55275c6b7a88791be5663f97d2e0ce7fe5a
Julien Depredurand <julien.depredurand@gmail.com>
2023-12-08 19:30:33 -0800

    day 2 part 1

--- day2/target/debug/deps/day2-2fbe421b055a2513.d
+++
@@ -1,5 +0,0 @@
-/home/bunker/projects/aoc2023/day2/target/debug/deps/libday2-2fbe421b055a2513.rmeta: src/main.rs
-
-/home/bunker/projects/aoc2023/day2/target/debug/deps/day2-2fbe421b055a2513.d: src/main.rs
-
-src/main.rs:
--- day2/target/debug/deps/day2-98c5d9316c4f1817.d
+++
@@ -1,5 +0,0 @@
-/home/bunker/projects/aoc2023/day2/target/debug/deps/libday2-98c5d9316c4f1817.rmeta: src/main.rs
-
-/home/bunker/projects/aoc2023/day2/target/debug/deps/day2-98c5d9316c4f1817.d: src/main.rs
-
-src/main.rs:
