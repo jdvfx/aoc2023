b81db565beba1a41c57cc52459b33dd041b2ae3e
Julien Depredurand <julien.depredurand@gmail.com>
2023-12-09 23:59:47 -0800

    found the bug, need to fix it

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -19,40 +19,44 @@ fn main() {
 
     let stream = fs::read_to_string("./input.txt").unwrap();
 
-//     let stream = "467..114..
-// ...*......
-// ..35..633.
-// ......#...
-// 617*......
-// .....+.58.
-// ..592.....
-// ......755.
-// ...$.*....
-// .664.598..";
+    let stream = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+-.592...22
+2.....755.
++..$.*....
+.664.598..";
 
+// that number 2 doesn't see the - and + above and below!!!
 
-    let mut c:Vec<Vec<char>> = Vec::new();
+    // char_points is a 2D_array (Vec<Vec<char>>) version of the input string
+    // parts store each characters and its x,y values, and if it's valid part (bool)
 
+    let mut char_points:Vec<Vec<char>> = Vec::new();
     let mut parts:Vec<Part>= Vec::new();
+
     let lines = stream.lines();
     for (y,l) in lines.enumerate(){
-        let mut nu=false;
+        let mut is_number=false;
         //
         let mut p:Vec<Point> = Vec::new();
         let mut num_str = "".to_string();
 
-        let mut ch:Vec<char> = Vec::new();
+        let mut char_point:Vec<char> = Vec::new();
         for (x,c) in l.chars().enumerate(){
 
-            ch.push(c);
+            char_point.push(c);
 
             if c.is_ascii_digit(){
-                nu = true;
+                is_number = true;
                 let p_ = Point{ x: x as i32, y: y as i32};
                 p.push(p_);
                 num_str.push_str(&c.to_string());
             }else{
-                if nu {
+                if is_number {
                     let part = Part{
                         num: num_str.parse::<u32>().unwrap_or(0),
                         valid: false,
@@ -62,26 +66,35 @@ fn main() {
                     p.clear();
                     num_str ="".to_string();
                 }
-                nu = false;
+                is_number = false;
             }
         }
-        c.push(ch);
+        char_points.push(char_point);
 
     }
 
+    for i in &parts{
+        println!("{:?}",i);
+    }
 
     // . . .
     // . X .
     // . . .
 
-    let around:Vec<[i32; 2]> = vec![[-1,-1],[-1,0],[1,0],[1,-1],[0,-1],[1,1],[0,1],[-1,1]];
+    // [-1,-1] [0,-1] [1,-1]
+    // [-1,0] X [1,0]
+    // [-1,1] [0,1] [1,1]
+
+    let around:Vec<[i32; 2]> = vec![[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]];
+
     //
     // for i in parts.iter(){
     //     println!("{:?}",i);
     // }
     //
-    let max_y = c.iter().len() as i32;
-    let max_x = c.iter().collect::<Vec<_>>().iter().len() as i32;
+    let max_y = char_points.iter().len() as i32;
+    let max_x = char_points.iter().collect::<Vec<_>>().iter().len() as i32;
+    // println!("{} {}",max_x,max_y);
     //
     let mut valid_parts_sum = 0;
     for i in parts{
@@ -90,12 +103,19 @@ fn main() {
                 let x = j.x + k[0] ;
                 let y = j.y + k[1] ;
                 if x>0 && y>0 && x<max_x && y<max_y{
+                    
                     let p = Point{ x, y };
-                    let ch = get_point(&c,p);
+                    // println!("{} {}",x,y);
+                    let ch = get_point(&char_points,p);
                     if !(ch.is_ascii_digit() || ch=='.'){
+                        // ifch.is_ascii()){
+                        // print!("{}",ch);
                         valid_parts_sum += i.num;
                         print!("{}{} ",i.num , ch);
                         break 'parts
+
+                    // }else{
+
                     }
                 }
             }
@@ -108,6 +128,7 @@ fn main() {
 // 1429 too low
 // 549292 too low
 
+//549292
 
 
 fn get_point(array2d: &Vec<Vec<char>> , point:Point ) -> char{
